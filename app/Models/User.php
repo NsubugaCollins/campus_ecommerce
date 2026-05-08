5da1635d663e3709896e84c126ad5eb643940585
NsubugaCollins <collinsnsubuga.ug@gmail.com>
2026-05-08 18:03:23 +0300

    Standardize User model fillable property for compatibility

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,18 +4,31 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'role', 'login_count', 'last_login_at', 'points', 'referral_code', 'referred_by'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name', 
+        'email', 
+        'password', 
+        'role', 
+        'login_count', 
+        'last_login_at', 
+        'points', 
+        'referral_code', 
+        'referred_by'
+    ];
+
+    protected $hidden = [
+        'password', 
+        'remember_token'
+    ];
 
     protected static function boot()
     {
