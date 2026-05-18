e9f589c29633fb6c37973f9350b7a71006971fbe
NsubugaCollins <collinsnsubuga.ug@gmail.com>
2026-05-18 21:00:49 +0300

    added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
 
         <!-- Center: Main Carousel -->
         <div class="col-lg-7 col-md-8">
-            <div id="heroCarousel" class="carousel slide hero-carousel" data-bs-ride="carousel">
+            <div id="heroCarousel" class="carousel slide hero-carousel" data-bs-ride="carousel" data-bs-interval="3000">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="0" class="active"></button>
                     <button type="button" data-bs-target="#heroCarousel" data-bs-slide-to="1"></button>
