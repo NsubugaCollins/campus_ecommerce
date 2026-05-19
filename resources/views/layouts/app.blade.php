2818d9356f99b927ec59e6fce71aba55493e9cc9
NsubugaCollins <collinsnsubuga.ug@gmail.com>
2026-05-19 20:21:48 +0300

    added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ $siteSettings['store_name'] ?? 'Cycle' }} - Premium E-Commerce</title>
+    <title>{{ $siteSettings['store_name'] ?? 'Cycle' }} - Sharing Economy Platform</title>
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 
     <!-- Bootstrap CSS -->
