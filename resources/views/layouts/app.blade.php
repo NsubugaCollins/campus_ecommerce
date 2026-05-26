e549d3e608a227c4673d1bb2f0e4d184c443816f
NsubugaCollins <collinsnsubuga.ug@gmail.com>
2026-05-26 19:59:12 +0300

    added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -267,5 +267,11 @@
             }
         });
     </script>
+    <!-- Floating Messages Button -->
+    <a href="{{ route('messages.index') }}" class="btn-floating-messages" aria-label="Messages">
+        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+        </svg>
+    </a>
 </body>
 </html>
